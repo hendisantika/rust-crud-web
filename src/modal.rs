9682781008ca572c04d608d804161a07ff8ea98d
hendisantika <hendisantika@yahoo.co.id>
2024-05-03 08:16:40 +0700

    Add ModalProperties🫸🌀✏️📗🐧🐳

--- src/modal.rs
+++ src/modal.rs
@@ -1,3 +1,12 @@
-use yew::{Component, ComponentLink, ShouldRender};
+use yew::{Callback, Component, ComponentLink, Properties, ShouldRender};
 use yew::services::ConsoleService;
 
+use crate::item::Item;
+
+#[derive(Properties, Clone)]
+pub struct ModalProperties {
+    pub item: Item,
+    pub visible: bool,
+    pub on_close: Callback<bool>,
+    pub on_save: Callback<Item>,
+}
